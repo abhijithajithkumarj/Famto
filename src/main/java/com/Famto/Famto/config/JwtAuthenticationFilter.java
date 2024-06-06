85bf37d02924c6b7a969326f8f69e4addbf54fc5
ABHIJITH <aabhivishnu@gmail.com>
2024-06-06 21:01:15 +0530

    second commit

--- src/main/java/com/Famto/Famto/config/JwtAuthenticationFilter.java
+++ src/main/java/com/Famto/Famto/config/JwtAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package com.Famto.Famto.config;
 
-
-
+import com.Famto.Famto.entity.Admin;
 import com.Famto.Famto.entity.User;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -21,36 +20,43 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-
     private final UserDetailsImp userDetailsService;
 
-    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsImp userDetails, UserDetailsImp userDetailsService) {
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsImp userDetailsService) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
-
     }
-
 
     @Override
     protected void doFilterInternal(
-       @NonNull     HttpServletRequest request,
-       @NonNull     HttpServletResponse response,
-       @NonNull     FilterChain filterChain) throws ServletException, IOException {
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        String authHeader=request.getHeader("Authorization");
+        String authHeader = request.getHeader("Authorization");
 
-        if (authHeader==null || !authHeader.startsWith("Bearer")){
-            filterChain.doFilter(request,response);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
             return;
         }
 
-        String token =authHeader.substring(7);
-        String username=jwtService.extractUsername(token);
-        if (username!=null && SecurityContextHolder.getContext().getAuthentication()==null){
-            UserDetails userDetails= userDetailsService.loadUserByUsername(username);
-            if (jwtService.isValid(token, (User) userDetails)){
-                UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(
-                        userDetails,null,userDetails.getAuthorities()
+        String token = authHeader.substring(7);
+        String username = jwtService.extractUsername(token);
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            boolean isValid = false;
+            if (userDetails instanceof Admin) {
+                isValid = jwtService.isValidAdmin(token, (Admin) userDetails);
+            } else if (userDetails instanceof User) {
+                isValid = jwtService.isValid(token, (User) userDetails);
+            }
+
+            if (isValid) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
                 );
                 authenticationToken.setDetails(
                         new WebAuthenticationDetailsSource().buildDetails(request)
@@ -58,8 +64,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
-        filterChain.doFilter(request,response);
-
+        filterChain.doFilter(request, response);
     }
-
 }
--- src/main/java/com/Famto/Famto/service/MerchantService.java
+++ src/main/java/com/Famto/Famto/service/MerchantService.java
@@ -3,8 +3,13 @@ package com.Famto.Famto.service;
 import com.Famto.Famto.dto.UserDto;
 import com.Famto.Famto.entity.User;
 
+import java.util.List;
+
 public interface MerchantService {
 
     UserDto saveMerchant(UserDto user);
+
+
+    List<User> userList ();
 
 }
